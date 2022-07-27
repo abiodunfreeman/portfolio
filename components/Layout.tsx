ae793a321ccaf43855745156b9b69d292caa2e81
Odun Freeman <bornduringwar@gmail.com>
2022-07-27 09:27:04 -0400

    .

--- components/Layout.tsx
+++
@@ -1,5 +0,0 @@
-import { NextPage } from 'next';
-import styles from '../styles/layout.module.css';
-const Layout: NextPage = ({ children }) => {
-  return <div id="layout-container">{children}</div>;
-};
